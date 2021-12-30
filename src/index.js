3170d27ad5602a4f661d601dc546dabeec8d0a75
Countess-Magpyr <91279492+Countess-Magpyr@users.noreply.github.com>
2021-12-30 11:15:55 +0100

    flex added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import { load } from "google-fonts-loader";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+load({ Roboto: ["400"] });
 
 ReactDOM.render(
   <React.StrictMode>
